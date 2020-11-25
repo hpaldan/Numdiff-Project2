61b26d80d88571004230bb76477cac258a2b1f2c
hpaldan <42342816+hpaldan@users.noreply.github.com>
2020-11-25 12:30:48 +0100

    File with the Sturm_Liouville algorithm

---
+++ sturm_liouville.m
@@ -0,0 +1,35 @@
+%Sturm_Liouville 
+
+%Defining all constants
+N = 400;
+alph = 0;
+bet = 1;
+dx = 1/(N+1);
+
+%Creating the grid
+f_vec = linspace(alph,bet,N+1);
+
+%From lecture slide 22/67
+Y_last = 
+
+
+R = [-2 1 zeros(1,N-2)];
+toep = 1/dx^2* toeplitz(R);
+[mode, A] =  eig(toep);
+
+eig_vals = [zeros(1,length(A))];
+
+string_vec = [zeros(1,length(A))];
+
+
+
+%%
+for i = 1:length(A)
+
+    plot(mode(:,i));
+    hold on
+
+end
+
+
+
